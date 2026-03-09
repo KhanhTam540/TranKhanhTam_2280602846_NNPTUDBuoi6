11988dd9379c32b10ccdf86de5722585af0c415c
KhanhTam540 <khanhtam540@gmail.com>
2026-03-09 10:55:00 +0700

    BaiTapBuoi6Ngay09032026

---
+++ controllers/users.js
@@ -0,0 +1,57 @@
+var express = require("express");
+let userModel = require("../schemas/users");
+let bcrypt = require('bcrypt');
+module.exports = {
+    CreateAnUser: async function (username, password,
+        email, role, fullName, avatarUrl, status
+    ) {
+        let newItem = new userModel({
+            username: username,
+            password: password,
+            email: email,
+            role: role,
+            fullName: fullName,
+            avatarUrl: avatarUrl,
+            status: status
+
+        });
+        await newItem.save();
+        return newItem;
+    },
+    FindByID: async function (id) {
+        return await userModel
+            .findOne({
+                _id: id,
+                isDeleted: false
+            }).populate({
+                path: 'role', select: 'name'
+            });
+    },
+    FindByUsername: async function (username) {
+        return await userModel.findOne(
+            {
+                username: username,
+                isDeleted: false
+            }
+        )
+    },
+    getAllUser: async function () {
+        let users = await userModel
+            .find({ isDeleted: false }).
+            populate({ path: 'role', select: 'name' })
+        return users;
+    },
+    ChangePassword: async function (userId, oldPassword, newPassword) {
+        let user = await userModel.findOne({ _id: userId, isDeleted: false });
+        if (!user) {
+            throw new Error("User khong ton tai");
+        }
+        let isMatch = bcrypt.compareSync(oldPassword, user.password);
+        if (!isMatch) {
+            throw new Error("Mat khau cu khong dung");
+        }
+        user.password = newPassword;
+        await user.save();
+        return user;
+    }
+}
